dc6fbfb4fa90626284b5c97acfe475dfbd1f7e63
rafajlz <frilo_94@hotmail.com>
2022-06-25 16:51:20 +0200

    Ruta protegida, comprobar si hay usuario conectado
    Si hay usuario puede ver lo que esta oculto, en este caso Inicio, sino lo envia al apartado de iniciar sesion

--- src/router.js
+++ src/router.js
@@ -1,17 +1,31 @@
-import {createRouter, createWebHistory } from 'vue-router';
-import Inicio from './views/Inicio.vue';
-import Registro from './views/Registro.vue';
-import InicioSesion from './views/InicioSesion.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import { useUserStore } from "./stores/user";
+import Inicio from "./views/Inicio.vue";
+import Registro from "./views/Registro.vue";
+import InicioSesion from "./views/InicioSesion.vue";
+
+// -> Antes de que entre a la ruta pasa por aqui con 'beforeEnter' <- //
+const requiereAuth = async (to, from, next) => {
+  // -> En este caso no se puede usar el store directamente fuera de los componentes, tiene que ser dentro de un metodo como este caso <- //
+  const userStore = useUserStore();
+  const user = await userStore.userActual();
+  // -> Si existe el user con next va a la ruta correspondiente(InicioSesion) y else lo contrario<- //
+  if (user) {
+    next();
+  } else {
+    next("/sesion");
+  }
+};
 
 const routes = [
-    {path: '/inicio', component: Inicio},
-    {path: '/registro', component: Registro},
-    {path: '/sesion', component: InicioSesion},
-]
+  { path: "/inicio", component: Inicio, beforeEnter: requiereAuth },
+  { path: "/registro", component: Registro },
+  { path: "/sesion", component: InicioSesion },
+];
 
 const router = createRouter({
-    routes,
-    history: createWebHistory()
-})
+  routes,
+  history: createWebHistory(),
+});
 
 export default router;
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,25 +1,26 @@
 // -> Creamos la tienda de Pina <- //
 import {
   createUserWithEmailAndPassword,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
 import { defineStore } from "pinia";
 import { auth } from "../hook/firebaseConfig";
 // -> Importamos el Router para poder usar el router.push() <- //
-import router from "../router"
+import router from "../router";
 
 export const useUserStore = defineStore("userStore", {
   state: () => ({
     userData: null,
-    cargandoUser: false
+    cargandoUser: false,
   }),
 
   // -> Con los action modificamos con funciones los datos principales del state <- //
   actions: {
     // -> Funcion para crear usuario autentificacion firebase <- //
     async registrarUsuario(email, password) {
-      this.cargandoUser = true
+      this.cargandoUser = true;
       try {
         // -> Destructuramos (el objeto tiene muchas posibilidades), sacamos la propiedad user, que seria la respuesta <-
         const { user } = await createUserWithEmailAndPassword(
@@ -39,7 +40,7 @@ export const useUserStore = defineStore("userStore", {
     },
     // -> Funcion para uniciar sesion de usuario creado en Firebase o registrado anteriormente <- //
     async inicioSesion(email, password) {
-      this.cargandoUser = true
+      this.cargandoUser = true;
       try {
         const { user } = await signInWithEmailAndPassword(
           auth,
@@ -52,18 +53,34 @@ export const useUserStore = defineStore("userStore", {
       } catch (error) {
         console.log(error);
       } finally {
-        this.cargandoUser = false
+        this.cargandoUser = false;
       }
     },
+    // -> Funcion que cierra la sesion del usuario de Firebase loggeado. <- //
     async cerrarSesion() {
       try {
         await signOut(auth);
         this.userData = null;
         //-> router.push() empuja a donde se quiera una vez se haya cerrado sesion <- //
-        router.push('/sesion')
+        router.push("/sesion");
       } catch (error) {
         console.log(error);
       }
+    },
+    // -> Funcion que verifica si hay un usuario conectado y que pueda ver la ruta protegida, en este caso Inicio/Home <- //
+    userActual() {
+      // -> Resuesta positiva(resovle), negativa(reject) <- //
+      return new Promise((resolve, reject) => {
+        const unsuscribe = onAuthStateChanged(auth, (user) => {
+          if (user) {
+            this.userData = { email: user.email, uid: user.uid };
+          } else {
+            this.userData = null;
+          }
+          resolve(user);
+        });
+        unsuscribe();
+      });
     },
   },
 });
